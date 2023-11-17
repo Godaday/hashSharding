1479e81e088d014f44c2534f3be66c74bd75b881
yanwf1 <yanwf1@centaline.com.cn>
2023-11-17 17:46:19 +0800

    init

---
+++ ConsistentHashRing.cs
@@ -0,0 +1,93 @@
+﻿using System.Security.Cryptography;
+
+namespace hashSharding;
+// 一致性哈希环类
+public class ConsistentHashRing
+{
+  private SortedDictionary<int, int> ring = new SortedDictionary<int, int>();
+
+    public List<int> Nodes => new List<int>(ring.Values);
+
+    public ConsistentHashRing(int initialNodeCount)
+    {
+        for (int i = 0; i < initialNodeCount; i++)
+        {
+            AddNode(i);
+        }
+    }
+
+    // 添加节点
+    public void AddNode(int newNode)
+    {
+        int hash = GetHash(newNode.ToString());
+
+        // 找到紧邻的两个节点
+        var successors = ring.Where(pair => pair.Key > hash).Take(2).ToList();
+
+        // 插入新节点到环上
+        ring[hash] = newNode;
+
+        if (successors.Count == 2)
+        {
+            // 将数据从后一个节点移动到新节点
+            MoveData(successors[0].Value, newNode);
+        }
+    }
+
+    // 移除节点
+    public void RemoveNode(int nodeToRemove)
+    {
+        int hashToRemove = GetHash(nodeToRemove.ToString());
+
+        // 找到要删除节点的后继节点
+        var successor = ring.FirstOrDefault(pair => pair.Key > hashToRemove);
+
+        if (successor.Key == 0 && successor.Value == 0)
+        {
+            // 要删除的节点是最后一个节点，找到环的第一个节点作为后继
+            successor = ring.First();
+        }
+
+        // 将后继节点的数据移到要删除的节点上
+        MoveData(successor.Value, nodeToRemove);
+
+        // 从环上移除节点
+        ring.Remove(hashToRemove);
+    }
+
+    // 获取数据在哈希环上的节点
+    public int GetNode(int data)
+    {
+        if (ring.Count == 0)
+        {
+            throw new InvalidOperationException("哈希环为空。");
+        }
+
+        int hash = GetHash(data.ToString());
+        var node = ring.FirstOrDefault(n => n.Key >= hash).Value;
+
+        return node.Equals(default(int)) ? ring.First().Value : node;
+    }
+
+    // 简单的哈希函数
+    private int GetHash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+            return BitConverter.ToInt32(hashBytes, 0);
+        }
+    }
+
+    // 移动数据
+    private void MoveData(int sourceNode, int destinationNode)
+    {
+        Console.WriteLine($"Moving data from Node {sourceNode} to Node {destinationNode}");
+
+        // 模拟数据移动：将源节点的数据移动到目标节点
+        foreach (var pair in ring.Where(entry => entry.Value == sourceNode).ToList())
+        {
+            ring[pair.Key] = destinationNode;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 模拟数据库表数据
+using hashSharding;
+
+List<int> data = new List<int> { 10, 25, 35, 50, 60, 75, 90 };
+
+        // 定义分片节点数量
+        int initialShardNodeCount = 3;
+
+        // 创建一致性哈希环
+        ConsistentHashRing hashRing = new ConsistentHashRing(initialShardNodeCount);
+
+        // 初始化数据分片
+        Dictionary<int, List<int>> shards = new Dictionary<int, List<int>>();
+        for (int i = 0; i < initialShardNodeCount; i++)
+        {
+            shards[i] = new List<int>();
+        }
+
+        // 初始化每个分片节点的数据列表
+        for (int i = 0; i < initialShardNodeCount; i++)
+        {
+            hashRing.AddNode(i);
+        }
+
+        // 使用一致性哈希进行数据分片
+        foreach (int value in data)
+        {
+            int shardNode = hashRing.GetNode(value);
+            shards[shardNode].Add(value);
+        }
+
+        // 打印初始分片结果
+        Console.WriteLine("Initial Shard Assignment:");
+        PrintShards(shards);
+
+        // 模拟增加节点
+        int addedNode = 3;
+        hashRing.AddNode(addedNode);
+
+
+        // 打印增加节点后的分片结果
+        Console.WriteLine($"\nAfter Adding Node {addedNode}:");
+        PrintShards(shards);
+
+        // 模拟删除节点
+        int removedNode = 1;
+        hashRing.RemoveNode(removedNode);
+
+
+
+        // 打印删除节点后的分片结果
+        Console.WriteLine($"\nAfter Removing Node {removedNode}:");
+        PrintShards(shards);
+
+
+// 打印分片结果
+static void PrintShards(Dictionary<int, List<int>> shards)
+{
+    foreach (var shard in shards)
+    {
+        Console.WriteLine($"Shard {shard.Key + 1}: " + string.Join(", ", shard.Value));
+    }
+}
+
+
